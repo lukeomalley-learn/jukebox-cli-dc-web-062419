0333122eb89e1c50dc743e0897139de9290888c3
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-05 19:54:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,9 +26,9 @@ end
 
 def play(songs)
   puts'Please enter a song name or number:'
-  input = gets.chomp
+  song_name = gets.chomp
   songs.each_with_index do |i, song|
-    if input == i.to_i + 1 || song
+    if song_name == i.to_i + 1 || song
       puts "Playing #{song}"
     else
       puts"Invalid input, please try again"
