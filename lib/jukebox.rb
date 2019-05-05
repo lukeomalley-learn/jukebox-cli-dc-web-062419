c5cb0272d341ce5ac28aacc552a158077c8684f2
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-05 19:36:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,5 +15,5 @@ def help
   puts'- help : displays this help message'
   puts'- list : displays a list of songs you can play'
   puts'- play : lets you choose a song to play'
-  puts'- help : displays this help message'
+  puts'- exit : exits this program'
 end
